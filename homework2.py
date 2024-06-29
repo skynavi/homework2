c7f5e0634b0f1672d2183d832dda14191d92ea4c
Sergey Ilin <56452788+skynavi@users.noreply.github.com>
2024-06-29 13:23:29 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+Number_of_complited_tasks=12 # 1 переменная
+Number_of_hours=1.5 # 2 переменная
+Name_of_course=('Python') # 3 переменная
+Time_per_task=(Number_of_hours/Number_of_complited_tasks) # вычисление 4 переменной
+print('Курс: '+ Name_of_course +', '+ 'всего задач:'+str(Number_of_complited_tasks)+', ' + 'затрачено часов: ' + str(Number_of_hours)+', ' +'среднее время выполнения ' + str(Time_per_task)+' часа.') #вывод результата
